b36a3039e980b7debd3816d338438fdb37bdad1e
YUGAL GAUR <yugalgaur174@gmail.com>
2024-10-27 19:55:55 +0530

    database mongodb connected

---
+++ src/index.js
@@ -0,0 +1,43 @@
+// require('dotenv').config({path:'./env'})
+// or by 
+import dotenv from "dotenv"
+
+import mongoose from "mongoose"; 
+import { DB_NAME } from "./constants.js";
+
+import connectDB from "./db/index.js";
+// import { path } from "express/lib/application";
+
+dotenv.config({
+    path:'./env'
+})
+
+connectDB()
+
+// import express from "express";
+// const app=express()
+
+// (async()=>{
+//     try{
+//         await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+//         app.on("error",(error)=>{
+//             console.log("ERROR in app: " ,error);
+//             throw err;
+//         })
+        
+//         app.listen(process.env.PORT,()=>{
+//             console.log("App is running on port: ",process.env.PORT);
+//         })
+//     }
+//     catch(error){
+//         console.log("Error:",error);
+//         throw error;
+//     }
+// })()
+
+// OR BY 
+// function connectDB(){
+
+// }
+
+// connectDB()
